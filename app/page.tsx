b90533440a4de8587f6ef132b7ab2232de238b28
mbring <mbringjerremy@gmail.com>
2023-12-09 17:23:02 +0100

    removing other unnecessery font

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import { Lusitana } from 'next/font/google';
 import '@/app/ui/home.module.css';
 import Image from 'next/image';
 
